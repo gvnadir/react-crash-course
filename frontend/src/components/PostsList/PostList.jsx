789474ddc211142fa6677aa390faf634403d0d7f
gvnadir <gianlucanadir@gmail.com>
2023-06-03 17:03:43 +0200

    init

---
+++ frontend/src/components/PostsList/PostList.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import NewPost from "../NewPost/NewPost";
+import Post from "../Post/Post";
+import Modal from "../Modal/Modal";
+import styles from "./PostsList.module.css";
+
+function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [...existingPosts, postData]);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={styles.posts}>
+          {posts.map((post) => (
+            <Post key={Date.now()} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostsList;
