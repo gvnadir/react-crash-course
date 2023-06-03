532588f3694991fb46645a7ad7ea51111f02397d
gvnadir <gianlucanadir@gmail.com>
2023-06-03 17:46:44 +0200

    add fetch and simulate BE service delay

--- frontend/src/components/PostsList/PostList.jsx
+++ frontend/src/components/PostsList/PostList.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import Modal from "../Modal/Modal";
 import styles from "./PostsList.module.css";
+import { URL } from "../../constants";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIseFetching] = useState(false);
+
+  useEffect(() => {
+    setIseFetching(true);
+    async function fetchPosts() {
+      const response = await fetch(`${URL}/posts`);
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIseFetching(false);
+    }
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
+    fetch(`${URL}/posts`, {
       method: "POST",
       body: JSON.stringify(postData),
       headers: {
@@ -25,17 +38,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post) => (
             <Post key={Date.now()} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
